2c32cf7ab7e05fc45401ada1000b5dc042317794
Fernando Tejerina <fertejerina97@gmail.com>
2022-11-29 17:56:20 -0300

    NavBar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,14 +3,16 @@ import './nav.css'
 import {AiFillHome} from 'react-icons/ai'
 import {FaUserAlt, FaUserGraduate} from 'react-icons/fa'
 import {IoMdMail} from 'react-icons/io'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#")
   return (
     <nav>
-      <a href="#" className='active'><AiFillHome/></a>
-      <a href="#about"><FaUserAlt/></a>
-      <a href="#experience"><FaUserGraduate/></a>
-      <a href="#contact"><IoMdMail/></a>
+      <a href="#" onClick={() => setActiveNav("#")} className={activeNav === "#" ? 'active' : ''}><AiFillHome/></a>
+      <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? 'active' : ''}><FaUserAlt/></a>
+      <a href="#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? 'active' : ''}><FaUserGraduate/></a>
+      <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? 'active' : ''}><IoMdMail/></a>
     </nav>
   )
 }
