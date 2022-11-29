5789adeda8566d067663368b90677042280478ef
Fernando Tejerina <fertejerina97@gmail.com>
2022-11-29 16:34:25 -0300

    Comienzo a trabajar en el componente nav

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Nav from './components/Nav/Nav'
 import About from './components/About/About'
 import Experience from './components/Experience/Experience'
 import Portfolio from './components/Portfolio/Portfolio'
-import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 
@@ -16,7 +15,6 @@ const App = () => {
       <About />
       <Experience />
       <Portfolio />
-      <Testimonials />
       <Contact />
       <Footer />
     </>
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './nav.css'
+import {AiFillHome} from 'react-icons/ai'
+import {FaUserAlt, FaUserGraduate} from 'react-icons/fa'
+import {IoMdMail} from 'react-icons/io'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" className='active'><AiFillHome/></a>
+      <a href="#about"><FaUserAlt/></a>
+      <a href="#experience"><FaUserGraduate/></a>
+      <a href="#contact"><IoMdMail/></a>
+    </nav>
   )
 }
 
