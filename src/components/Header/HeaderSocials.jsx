fbd76f6ec9f5b0943b8ee7cb05a4dc5944c9d330
Fernando Tejerina <fertejerina97@gmail.com>
2022-12-03 12:05:58 -0300

    Se añadio el link correspondiente en el componente HeaderSocials

--- src/components/Header/HeaderSocials.jsx
+++ src/components/Header/HeaderSocials.jsx
@@ -4,9 +4,9 @@ import {BsLinkedin, BsGithub, BsInstagram} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-       <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a> 
-       <a href="https://github.com" target="_blank"><BsGithub/></a> 
-       <a href="https://instagram.com" target="_blank"><BsInstagram/></a> 
+       <a href="https://www.linkedin.com/in/fernando-javier-tejerina-arg" target="_blank"><BsLinkedin/></a> 
+       <a href="https://github.com/fertejj" target="_blank"><BsGithub/></a> 
+       <a href="https://www.instagram.com/fertejerina97" target="_blank"><BsInstagram/></a> 
     </div>
   )
 }
