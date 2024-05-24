bb534eb8a312051ab1f46c795aa2f4b195f870f7
Fernando Javier Tejerina <fertejerina97@gmail.com>
2024-05-24 00:46:16 -0300

    agrego proyecto a portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,12 +2,23 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from "../../assets/portfolio1.png"
 import IMG2 from "../../assets/portfolio2.png"
+import IMG3 from "../../assets/portfolio3.png"
 
 const Portfolio = () => {
   return (
     <section id='portfolio'>
       <h5>Mis trabajos recientes</h5>
       <h2>Portfolio</h2>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG3} alt="Portfolio 2" />
+            <h3>Solutec LandingPage</h3>
+          </div>
+          <div className="portfolio__item-cta">
+          <a href="https://github.com/fertejj/LANDING-SOLUTEC" className='btn' target="_blank">Repository</a>
+          <a href="https://www.solutecjuy.com/" className='btn btn-primary' target="_blank">Live Demo</a>
+          </div>
+        </article>
 
       <div className="container portfolio__container">
         <article className='portfolio__item'>
@@ -31,6 +42,7 @@ const Portfolio = () => {
           <a href="https://fertejj.github.io/mobile-first-layout-course/" className='btn btn-primary' target="_blank">Live Demo</a>
           </div>
         </article>
+
       </div>
     </section>
   )
